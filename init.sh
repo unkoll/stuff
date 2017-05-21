bc98377ab5a05613916475082d0578885d5334e1
unkoll <unkoll>
2017-05-21 14:58:23 +0300

    1.9.11 attempt 3

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/task.conf
 sudo unlink /etc/nginx/sites-enabled/default
-echo 'Reference nginx : complete'
-echo 'Restartinh nginx...'
-sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
-echo 'Restarting gunicorn...'
-sudo /etc/init.d/gunicorn restart
+sudo service nginx restart
+cd /home/box/web
+gunicorn -b 0.0.0.0:8080 hello &
+sudo service gunicorn restart
