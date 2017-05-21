977b55fe25feda1ab663e34698ef5158c70c8079
unkoll <unkoll>
2017-05-21 19:54:51 +0300

    2.1.11 attempt 4

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/task.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo service nginx restart
 cd /home/box/web
-sudo gunicorn -b 0.0.0.0:8080 hello:app &
-sudo gunicorn -c 0.0.0.0:8000 ask.ask.wsgi:app &
+sudo gunicorn -b 0.0.0.0:8080 hello:application &
+cd ask/ask
+sudo gunicorn -b 0.0.0.0:8000 wsgi:application &
