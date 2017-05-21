f9de9897e9754c1106014e41a15ee10f180f7064
unkoll <unkoll>
2017-05-21 14:15:57 +0300

    1.9.11 attempt 2 with cheating

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/task.conf
 sudo unlink /etc/nginx/sites-enabled/default
+echo 'Reference nginx : complete'
+echo 'Restartinh nginx...'
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/task.conf
+sudo gunicorn -b 0.0.0.0:8080 hello:app
+echo 'Restarting gunicorn...'
 sudo /etc/init.d/gunicorn restart
